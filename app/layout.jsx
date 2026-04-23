94d7ad3d95cfbc88b723d8327f93b5b07949e7aa
andretomazela-dev <andre.tomazela@gmail.com>
2026-04-23 17:07:03 -0300

    Update layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,18 +1,8 @@
-import "./globals.css";
-import Header from "@/components/Header";
-
 export default function RootLayout({ children }) {
   return (
-    <html lang="pt-BR">
-      <body className="bg-white text-gray-900">
-
-        <Header />
-
-        {/* ✅ SÓ ISSO resolve o problema */}
-        <main className="pt-[110px] md:pt-[130px]">
-          {children}
-        </main>
-
+    <html lang="pt-br">
+      <body>
+        {children}
       </body>
     </html>
   );
