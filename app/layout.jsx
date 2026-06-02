fbe45c8baa8ffea5f586b46fac5a5b5ca6a591e3
andretomazela-dev <andre.tomazela@gmail.com>
2026-06-02 12:57:54 -0300

    Update layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -4,17 +4,30 @@ import Analytics from "./analytics";
 
 export const metadata = {
   title: "Tomazela | Estratégia & Comunicação",
+
   description:
     "Comunicação sob medida para marcas e organizações de impacto.",
+
   icons: {
     icon: [
-      { url: "/favicon.ico" },
-      { url: "/favicon-32x32.png", sizes: "32x32" },
-      { url: "/favicon-16x16.png", sizes: "16x16" },
+      {
+        url: "/favicon/tomazela-favicon.ico",
+      },
+      {
+        url: "/favicon/tomazela-favicon-32x32.png",
+        sizes: "32x32",
+        type: "image/png",
+      },
+      {
+        url: "/favicon/tomazela-favicon-16x16.png",
+        sizes: "16x16",
+        type: "image/png",
+      },
     ],
-    apple: [{ url: "/apple-touch-icon.png", sizes: "180x180" }],
   },
+
   manifest: "/site.webmanifest",
+
   themeColor: "#FF4D00",
 };
 
