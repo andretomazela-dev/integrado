e2d4ebb7f52b09a2f8e56cf22049503a530cfb97
andretomazela-dev <andre.tomazela@gmail.com>
2026-06-03 10:49:26 -0300

    Update layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -9,32 +9,6 @@ export const metadata = {
   description:
     "Comunicação sob medida para marcas e organizações de impacto.",
 
-  icons: {
-    icon: [
-      {
-        url: "/favicon/tomazela-favicon.ico",
-      },
-      {
-        url: "/favicon/tomazela-favicon-32x32.png",
-        sizes: "32x32",
-        type: "image/png",
-      },
-      {
-        url: "/favicon/tomazela-favicon-16x16.png",
-        sizes: "16x16",
-        type: "image/png",
-      },
-    ],
-
-    apple: [
-      {
-        url: "/favicon/tomazela-favicon-128x128.png",
-        sizes: "128x128",
-        type: "image/png",
-      },
-    ],
-  },
-
   manifest: "/site.webmanifest",
 
   themeColor: "#FF4D00",
